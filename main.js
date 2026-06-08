466b5eb37528a16b0cd45acf9ac107cfccbfe341
Eesa Khan <eesak490@gmail.com>
2026-06-08 14:12:39 +0530

    anged a lil bug in code and enhanced the ui

--- main.js
+++ main.js
@@ -1,15 +1,25 @@
 const { BrowserWindow, app } = require("electron");
+const path = require("path");
 
 function createWindow() {
     const win = new BrowserWindow({
         width: 1200,
         height: 800,
+        backgroundColor: "#0f1117",
         titleBarStyle: "hiddenInset",
-        backgroundColor: "#0f1117"
-        
+
+        webPreferences: {
+            preload: path.join(__dirname, "preload.js")
+        }
     });
 
     win.loadFile("index.html");
 }
 
 app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
--- script.js
+++ script.js
@@ -5,38 +5,31 @@ const currentTask = document.getElementById("currentTask");
 let currentActivity = "";
 let seconds = 0;
 let timer = null;
-
-startBtn.addEventListener("click", () => {
-
+    startBtn.addEventListener("click", () => {
     currentActivity = taskInput.value;
-
     if (currentActivity === "") {
         return;
     }
-
+    taskInput.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        startBtn.click();
+    }
+});
     currentTask.textContent = currentActivity;
-
+    taskInput.value = "";
     seconds = 0;
-
     clearInterval(timer);
-
     timer = setInterval(() => {
-
         seconds++;
-
         const hrs = String(Math.floor(seconds / 3600)).padStart(2, "0");
-
         const mins = String(
             Math.floor((seconds % 3600) / 60)
         ).padStart(2, "0");
-
         const secs = String(
             seconds % 60
         ).padStart(2, "0");
-
         currentTask.textContent =
-            `
-        ${currentActivity} | ${hrs}:${mins}:${secs}`;
+        `${currentActivity} | ${hrs}:${mins}:${secs}`;
 
     }, 1000);
 
